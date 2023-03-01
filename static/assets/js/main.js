d5243b7582b87cd128f6ca583e52a39017207c99
Tejasvw <tejaswarukar24@gmail.com>
2023-03-01 23:55:49 +0530

    Responsiveness added to home image slider

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -65,14 +65,60 @@ const ref = ()=>{
   }
 
   // js for image slider
-  var counter = 1;
-        setInterval(function(){
-          document.getElementById('radio' + counter).checked = true;
-          counter++;
-          if(counter > 4){
-            counter = 1;
-          }
-        }, 4000);
+  
+    var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
+
+    // Javascript for image slider manual navigation
+    var manualNav = function(manual){
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      });
+
+      slides[manual].classList.add('active');
+      btns[manual].classList.add('active');
+    }
+
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+      });
+    });
+
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
+
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 5000);
+      }
+      repeater();
+    }
+    repeat();
+   
 
 
 
